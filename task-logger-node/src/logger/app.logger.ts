ecfb725e3a898bdda17a55ade2fcc1ed47b5d5f7
aulikes <aulikes@hotmail.com>
2025-07-18 04:32:47 -0500

    ELK + Filebeat

--- task-logger-node/src/logger/app.logger.ts
+++ task-logger-node/src/logger/app.logger.ts
@@ -1,20 +1,32 @@
 import { Injectable, LoggerService } from '@nestjs/common';
 import * as winston from 'winston';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppLogger implements LoggerService {
   private readonly logger: winston.Logger;
 
   constructor() {
+    // Asegura que el directorio 'logs' exista
+    const logDir = path.resolve(__dirname, '..', '..', 'logs');
+    if (!fs.existsSync(logDir)) {
+      fs.mkdirSync(logDir);
+    }
+
     this.logger = winston.createLogger({
       level: 'info',
       format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
+        winston.format.timestamp(),            // Agrega timestamp
+        winston.format.json()                  // Salida en formato JSON estructurado
       ),
-      defaultMeta: { service: 'task-logger-node' },
+      defaultMeta: { service: 'task-logger-node' }, // Cambia este nombre según el microservicio
       transports: [
-        new winston.transports.Console()
+        new winston.transports.Console(),
+        new winston.transports.File({
+          filename: path.join(logDir, 'app.log'), // Salida a archivo ./logs/app.log
+          level: 'info'
+        }),
       ],
     });
   }
